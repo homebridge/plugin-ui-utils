d6c4da594019ecda666ec506efabf3253eb39f63
Kovalovszky Patrik <kovapatrik@gmail.com>
2026-03-04 22:32:18 +0100

    chore: use single quote

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { HomebridgePluginUiServer, RequestError } from './server.js'
-export type { IHomebridgePluginUi, IHomebridgeUiFormHelper, IHomebridgeUiToastHelper } from "./ui.interface.js"
+export type { IHomebridgePluginUi, IHomebridgeUiFormHelper, IHomebridgeUiToastHelper } from './ui.interface.js'
